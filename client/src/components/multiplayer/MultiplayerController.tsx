52e356cbcc66fb54c4c4af4d3af7b34fa7c39b43
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2024-03-14 11:50:38 -0400

    progress on multiplayer debugging

--- client/src/components/multiplayer/MultiplayerController.tsx
+++ client/src/components/multiplayer/MultiplayerController.tsx
@@ -1,8 +1,5 @@
-import React, { useState, useEffect, lazy } from 'react';
+import React, { useState, useEffect } from 'react';
 import QuestionRenderer from '../renderers/QuestionRenderer';
-// import Chat from './Chat';
-// import QuestionsMultiplayer from './QuestionsMultiplayer';
-// import io from 'socket.io-client';
 import { useMutation, useQuery } from 'convex/react';
 import { api } from '../../../convex/_generated/api';
 
@@ -19,62 +16,59 @@ interface QuestionSchema {
   correctAnswer: string;
 }
 
-// const socket = io.connect('http://localhost:3001');
-
 const MultiplayerController = ({
   loadQuestions,
   handleCorrectAnswerSelected,
   handleNextQuestion,
 }) => {
   const createRoom = useMutation(api.rooms.createRoom);
-  const waitingRoom = useQuery(api.rooms.getWaitingRoom);
-  const updateRoomCapacity = useMutation(api.rooms.updateRoomCapacity);
-  const [roomID, setRoomID] = useState<string>('');
-  // const updatedRoom = useQuery(api.rooms.getRoom, { id: roomID });
+  const waitingRoomQuery = useQuery(api.rooms.getWaitingRoom);
+  // const updatedWaitingRoom = useQuery(api.rooms.getRoom, {
+  //   id: waitingRoomQuery._id,
+  // });
 
+  const [updateRoom, setUpdateRoom] = useState<boolean>(false);
+  const [clientReady, setClientReady] = useState<boolean>(false);
+
+  // useEffect(() => {
+  //   if (waitingRoomQuery && updateRoom) {
+  //     const updatedWaitingRoom = useQuery(api.rooms.getRoom, {
+  //       id: waitingRoomQuery._id,
+  //     });
+
+  //     if (updatedWaitingRoom) {
+  //       console.log(updatedWaitingRoom.player1Ready);
+  //       console.log(updatedWaitingRoom.player2Ready);
+  //       setClientReady(
+  //         updatedWaitingRoom.player1Ready && updatedWaitingRoom.player2Ready
+  //       );
+  //     }
+  //   }
+  // }, [updateRoom, waitingRoomQuery]);
+
+  const updateRoomCapacity = useMutation(api.rooms.updateRoomCapacity);
   const [questions, setQuestions] = useState<string[]>([]);
   const [options, setOptions] = useState<string[][]>([]);
   const [correctAnswers, setCorrectAnswers] = useState<string[]>([]);
-  const [clientReady, setClientReady] = useState<boolean>(false);
-
   const [player1_score, setPlayer1Score] = useState<number>(0);
   const [player2_score, setPlayer2Score] = useState<number>(0);
-
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
   const [startGame, setStartGame] = useState<boolean>(false);
-
   const [playerWaiting, setPlayerWaiting] = useState<boolean>(false);
-  const [players, setPlayers] = useState<number>(0);
-
-  // const handleAnswerSelect = (option: string) => {
-  //   setSelectedAnswer(option);
-  //   if (option === correctAnswer) {
-  //     onCorrectAnswerSelected();
-  //   }
-  // };
 
   const joinRoom = async () => {
-    // check if room already exists, will have to query for rooms
-    // with at least for for capacity.
-
     try {
-      // Load questions into it randomly from question table.
-      // TODO Create new field to hold that information.
-      if (waitingRoom) {
+      if (waitingRoomQuery) {
         await updateRoomCapacity({
-          id: waitingRoom._id,
+          id: waitingRoomQuery._id,
         });
 
-        setQuestions(waitingRoom.questions);
-        setOptions(waitingRoom.options);
-        setCorrectAnswers(waitingRoom.correctAnswers);
-
-        // if (updatedWaitingRoom) {
-        //   setClientReady(
-        //     updatedWaitingRoom.player1Ready && updatedWaitingRoom.player2Ready
-        //   );
-        // }
+        setQuestions(waitingRoomQuery.questions);
+        setOptions(waitingRoomQuery.options);
+        setCorrectAnswers(waitingRoomQuery.correctAnswers);
+        console.log(waitingRoomQuery.player1Ready);
+        console.log(waitingRoomQuery.player2Ready);
       } else {
         const newRoom = {
           player1_score,
@@ -85,26 +79,13 @@ const MultiplayerController = ({
         };
 
         createRoom(newRoom);
+        setUpdateRoom(false);
       }
     } catch (error) {
       console.log(`Error creating the room: ${error}`);
       throw error;
     }
-    // if (username !== '' && room !== '') {
-    //   await socket.emit('join_room', room);
-    // } else {
-    //   console.log('Username and room must be provided');
-    // }
   };
-
-  useEffect(() => {
-    // const updatedRoom = useQuery(api.rooms.getRoom, {});
-    if (waitingRoom) {
-      console.log(waitingRoom.player1Ready);
-      console.log(waitingRoom.player2Ready);
-      setClientReady(waitingRoom.player1Ready && waitingRoom.player2Ready);
-    }
-  }, [waitingRoom, clientReady]);
 
   useEffect(() => {
     if (loadQuestions) {
@@ -125,28 +106,7 @@ const MultiplayerController = ({
       setOptions(optionsHolder);
       setCorrectAnswers(correctAnswersHolder);
     }
-
-    // socket.on('room_status', ({ players, maxPlayers }: RoomCapacity) => {
-    //   console.log(players, maxPlayers);
-    //   setPlayers(players);
-    // });
-    // socket.on('waiting_for_player', () => {
-    //   const waitingWarning = `There needs to be at least 2 players to start a game. Currently the number of players is ${players}`;
-    //   console.log(waitingWarning);
-    //   setPlayerWaiting(waitingWarning);
-    // });
-    // socket.on('start_game', () => {
-    //   console.log(`${players} players have joined the lobby. Starting game...`);
-    //   setPlayerWaiting('');
-    //   setStartGame(true);
   }, [loadQuestions]);
-
-  //   return () => {
-  //     socket.off('room_status');
-  //     socket.off('waiting_for_player');
-  //     socket.off('start_game');
-  //   };
-  // }, [socket]);
 
   return (
     <div>
