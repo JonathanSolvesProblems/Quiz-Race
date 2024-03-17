54d9e75fd98b64078bcbd6e0cb8e7fec606e28ce
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2024-03-17 14:26:15 -0400

    added back bootstrap import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ConvexProvider, ConvexReactClient } from 'convex/react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
